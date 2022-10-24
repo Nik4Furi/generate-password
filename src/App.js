f821ad98530c0e82959ba4f5d9b0b32f37529251
Torrent <torrent23@gmail.com>
2022-10-24 09:50:05 +0530

    Add to into github

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css'
 import InputBox from './Components/InputBox';
 
 function App() {
--- src/Components/InputBox.js
+++ src/Components/InputBox.js
@@ -1,3 +1,4 @@
+import '../App.css'
 import React, { useState } from 'react'
 import Copy from '../copy.svg'
 
